01d87cc22b27bfc82f78e49c16253f1eb19badc6
sulakshana sraavya <sulakshanasraavya1@gmail.com>
2025-09-08 16:31:49 +0530

    Add Nutritional goals feature

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 from dotenv import load_dotenv
 import os
-from llama_cpp import Llama
+from groq import Groq
+import json
 
 load_dotenv()
 
 usda_api_key = os.getenv("USDA_API_KEY")
+groq_api_key = os.getenv("GROQ_API_KEY")
 
 search_url = "https://api.nal.usda.gov/fdc/v1/foods/search"
 
@@ -52,47 +54,65 @@ def fetch_data(food_items):
 
     return food_data
 
-
-def main():
-    age = 20
-    gender = "female"
-    goal = "weight_loss"
-    diet_preference = "vegetarian"
-    allergies = ["peanuts"]
-    calorie_target = 1800
-
-    user_preference = {
-        "age": age,
-        "gender": gender,
-        "goal": goal,
-        "diet_preference": diet_preference,
-        "allergies": allergies,
-        "calorie_target": calorie_target
+def user_input():
+    food_items = ["apple", "banana", "orange", "honey", "chips"]
+    user_preferences = {
+        "age": 20,
+        "gender": "female",
+        "goal": "weight_loss",
+        "diet_preference": "vegetarian",
+        "allergies": ["peanuts"],
+        "calorie_target": 1800
     }
+    return food_items, user_preferences
 
-    user_input = input("Enter comma separated food items to track your nutrients intake: ")
-    food_items = [input.strip() for input in user_input.split(',')]
-    print(food_items)
+def get_nutrient_goals(food_items, user_preferences):
+    with open('nutritional_goals.json', 'r') as file:
+        nutrient_goals = json.load(file)
+
     food_data = fetch_data(food_items)
-    print(food_data)
-    print(user_preference)
 
-    prompt = f"""
-    Here are the list of food items:
-    {food_items}
-    and here are the user preferences:
-    {user_preference}
-    Now analyze these inputs and check for any dietary restrictions or suggestions.
-    """
+    categories = ["child", "female"]
+    user_gender = user_preferences.get("gender")
+    age = user_preferences.get("age")
+    age_groups = []
+    for category in categories:
+        age_groups.extend(age for age in nutrient_goals['data'][category].keys())
+    
+    for group in age_groups:
+        if '-' in group:
+            start, end = group.split('-')
+            if age >= int(start) and age <= int(end):
+                user_age = group
+                break
+        elif '+' in group:
+            start = group.replace('+', '')
+            if age >= int(start):
+                user_age = group
+                break
+    nutrient_data = nutrient_goals['data'][user_gender][user_age]
+    
+    return food_data, nutrient_data
+def main():
+    food_items, user_preferences = user_input()
+    food_data, nutrient_data = get_nutrient_goals(food_items, user_preferences)
 
-    llm = Llama.from_pretrained(
-	repo_id="sfardin/diet_AI_model_gguf",
-	filename="unsloth.Q4_K_M.gguf",
-    verbose=False
+    client = Groq(api_key = groq_api_key)
+    completion = client.chat.completions.create(
+        model = "llama-3.3-70B-Versatile",
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a helpful nutrition assistant. You help users understand their dietary intake and make them understand if it aligns with their nutritional goals and preferences."
+            },
+            {
+                "role": "user",
+                "content": f"Here is my food intake data for today: {food_data}. My preferences are: {user_preferences}. And the required amount of nutrients required based on gender and age are {nutrient_data} Based on this, provide me with a summary of my nutrient intake and suggest dietary recommendations to help me achieve my health goals."
+            }
+        ]
     )
+    print(completion.choices[0].message.content)
 
-    output = llm(prompt)
-    print(output["choices"][0]["text"])
 
 if __name__ == "__main__":
     main()
